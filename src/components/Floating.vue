c559fe6ef981b4ea9fafb52c44f6559f30e4dc99
Duarte David <deltaduartedavid@gmail.com>
2017-09-18 18:13:46 +0200

    Dont process mousemove when an element is fullscreen (Fix #1) and move floating on resize

--- src/components/Floating.vue
+++ src/components/Floating.vue
@@ -47,21 +47,36 @@ export default {
       }
     },
     onMouseMove(e) {
-      if (this.dragged) {
+      if (this.dragged && document.fullscreenElement === null) {
         this.position = {
           left: `${e.clientX - this.offset.x}px`,
           top: `${e.clientY - this.offset.y}px`
         }
+      }
+    },
+    onResize() {
+      if (this.position) {
+        this.position = {
+          left: Math.round((parseInt(this.position.left) / this.oldWindowSize.w) * window.innerWidth) + 'px',
+          top: Math.round((parseInt(this.position.top) / this.oldWindowSize.h) * window.innerHeight) + 'px'
+        }
+      }
+      this.oldWindowSize = {
+        w: window.innerWidth,
+        h: window.innerHeight
       }
     }
   },
   created() {
     document.addEventListener('mouseup', this.onMouseUp)
     document.addEventListener('mousemove', this.onMouseMove)
+    window.addEventListener('resize', this.onResize)
+    this.onResize()
   },
   destroyed() {
     document.removeEventListener('mouseup', this.onMouseUp)
     document.removeEventListener('mousemove', this.onMouseMove)
+    window.removeEventListener('resize', this.onResize)
   }
 }
 </script>
