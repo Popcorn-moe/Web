73b63d322c134aadbd4bdab0ba0bd9b0a16dd074
Duarte David <deltaduartedavid@gmail.com>
2017-10-26 17:38:30 +0200

    Use real data for search

--- src/components/layout/Search.vue
+++ src/components/layout/Search.vue
@@ -14,30 +14,35 @@
           prepend-icon="search"
           hide-details
           single-line
+          v-model="query"
         ></v-text-field>
       </v-toolbar>
-      <v-list class="search-results">
-        <v-subheader inset>Users</v-subheader>
-        <v-list-tile avatar v-for="item in users" :key="item.title">
-          <v-list-tile-avatar>
-            <img :src="item.avatar">
-          </v-list-tile-avatar>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-divider inset></v-divider>
-        <v-subheader inset>Animes</v-subheader>
-        <v-list-tile v-for="item in animes" :key="item.title" avatar>
-          <v-list-tile-avatar>
-            <img :src="item.banner">
-          </v-list-tile-avatar>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
+      <v-list class="search-results" v-if="result.users.length || result.animes.length">
+        <template v-if="result.users.length">
+          <v-subheader inset>Users</v-subheader>
+          <v-list-tile avatar v-for="user in result.users" :key="user.id">
+            <v-list-tile-avatar>
+              <img :src="user.avatar">
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ user.login }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ user.status }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+        <v-divider inset v-if="result.users.length && result.animes.length"></v-divider>
+        <template v-if="result.animes.length">
+          <v-subheader inset>Animes</v-subheader>
+          <v-list-tile v-for="anime in result.animes" :key="anime.id" avatar>
+            <v-list-tile-avatar>
+              <img :src="anime.cover">
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ anime.names[0] }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ anime.release_date }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
       </v-list>
     </div>
     <v-btn v-else @click.stop="toggled = true" fab class="main-color" small>
@@ -50,20 +55,18 @@
 import { VSlideXTransition } from 'vuetify/es5/components/transitions'
 import { VBtn, VIcon, VTextField, VToolbar, VDivider, VSubheader, VAvatar } from 'vuetify/es5/components'
 import { VList, VListGroup, VListTile, VListTileTitle, VListTileContent, VListTileAvatar, VListTileSubTitle } from 'vuetify/es5/components/VList'
+import gql from 'graphql-tag'
 
 export default {
   name: 'Search',
   data() {
     return {
       toggled: false,
-      users: [
-        { title: 'Loucassoulé', subtitle: 'Le bon cassoulé ça mere', avatar: 'https://cdn.discordapp.com/avatars/175646285456474112/b959f0e240c9653d91bae27be8d9eecd.png' },
-        { title: 'Xx_SkyBeast_xX', subtitle: 'Watching a loli', avatar: 'https://cdn.discordapp.com/avatars/151023528446001152/9792d2aed4c2a5d08ceec3588d2b3396.png' },
-        { title: 'DeltaEmo', subtitle: 'J\'aime les licornes', avatar: 'https://cdn.discordapp.com/avatars/107942335274307584/a_0e66b32131625a6c3a82be6ccd012b7c.png' }
-      ],
-      animes: [
-        { title: 'Sword Art Online', subtitle: 'Jan 20, 2014', banner: 'https://api.popcorn.moe/uploads/029fa65080116a9414b684ab61f86a6e.jpeg' }
-      ]
+      query: '',
+      result: {
+        users: [],
+        animes: []
+      }
     }
   },
   components: {
@@ -82,7 +85,25 @@ export default {
     VListTileContent,
     VListTileAvatar,
     VListTileSubTitle
-  }
+  },
+  apollo: {
+    result: {
+      query: gql`query ($name: String!) {
+        users: searchUser(name: $name, limit: 2) {
+          id login avatar
+        }
+        animes: searchAnime(name: $name, limit: 2) {
+          id names cover release_date
+        }
+      }`,
+      variables() {
+        return {
+          name: this.query || ''
+        }
+      },
+      update: data => data
+    }
+  },
 }
 </script>
 
