22c5c99accca76155609478d39929187c6bd908a
Duarte David <deltaduartedavid@gmail.com>
2018-03-01 23:30:07 +0100

    Add blur on image loading

--- src/components/anime/Anime.vue
+++ src/components/anime/Anime.vue
@@ -2,7 +2,7 @@
     <div class="anime text-xs-center">
         <router-link :to="{ name: 'Anime', params: { id: value.id }}">
             <div class="cover-container">
-                <img class="cover" alt="cover" :src="value.cover">
+                <p-img class="cover" alt="cover" :src="value.cover"/>
                 <v-icon class="arrow">play_arrow</v-icon>
             </div>
         </router-link>
@@ -22,6 +22,7 @@
 <script>
 import { VIcon } from "vuetify/es5/components";
 import Rating from "./Rating";
+import PImg from "../ProgressiveImg";
 
 export default {
 	props: ["value"],
@@ -32,7 +33,8 @@ export default {
 	},
 	components: {
 		VIcon,
-		Rating
+		Rating,
+		PImg
 	}
 };
 </script>
