04c1432ff5e2bece6bf10161857e2de760f6083d
loucass003 <loucass003@gmail.com>
2018-03-29 20:31:32 +0200

    Add space between content and footer

--- src/components/layout/Layout.vue
+++ src/components/layout/Layout.vue
@@ -10,7 +10,7 @@
         <mobile-navbar v-else></mobile-navbar>
 
         <main>
-            <v-content>
+            <v-content class="mb-3">
                 <loader v-show="isLoading"></loader>
                 <div v-show="!isLoading">
                     <v-slide-y-transition mode="out-in">
