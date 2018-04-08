a6f3afa3ec5f6871b50b29508a021dbf90df3df5
Duarte David <deltaduartedavid@gmail.com>
2018-04-08 12:45:44 +0200

    Use vuetify elements for the footer

--- src/components/layout/LayoutFooter.vue
+++ src/components/layout/LayoutFooter.vue
@@ -1,66 +1,85 @@
 <template>
 	<v-footer class="footer pt-3" absolute inset app height="auto" ref="footer">
-		<v-container grid-list-md>
+		<v-container grid-list-md class="pb-0">
 			<v-layout row wrap>
 				<v-flex sm6 lg4>
 					<h3>A propos</h3>
-					<p>
+					<p class="text-xs-justify">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 					</p>
 				</v-flex>
-				<v-flex sm6 lg8>
-					<div class="text-xs-center follow">
+				<v-flex sm6 lg8 class="text-xs-center">
+					<div class="vertical-center">
 						<h1>Follow us</h1>
-						<a 
-							class="primary social elevation-3"
+						<v-btn
+							fab
+							class="mx-3"
+							color="primary"
 							href="https://twitter.com/popcorn_moe"
 							rel="noopener"
 							target="_blank"
 						>
-							 <img src="/static/icons/twitter-icon.svg" height="35px">
-						</a>
-						<a 
-							class="primary social elevation-3"
+								<img src="/static/icons/twitter-icon.svg" height="35px">
+						</v-btn>
+						<v-btn
+							fab
+							class="mx-3"
+							color="primary"
 							href="https://discord.gg/FEvvcWW"
 							rel="noopener"
 							target="_blank"
 						>
-							 <img src="/static/icons/discord-icon.svg" height="35px">
-						</a>
-						<a 
-							class="primary social elevation-3"
+								<img src="/static/icons/discord-icon.svg" height="35px">
+						</v-btn>
+						<v-btn
+							fab
+							class="mx-3"
+							color="primary"
 							href="https://github.com/Popcorn-moe"
 							rel="noopener"
 							target="_blank"
 						>
-							 <img src="/static/icons/github-icon.svg" height="35px">
-						</a>
+								<img src="/static/icons/github-icon.svg" height="35px">
+						</v-btn>
 					</div>
 				</v-flex>
 			</v-layout>
-			<div class="text-xs-center links">
-				<ul>
-					<li>Mentions légales</li>
-					<li>CGU</li>
-					<li>A propos</li>
-				</ul>
+			<v-layout row wrap justify-center>
+				<v-btn flat>
+					Mentions légales
+				</v-btn>
+				<v-btn flat>
+					CGU
+				</v-btn>
+				<v-btn flat>
+					A propos
+				</v-btn>
+			</v-layout>
+			<div class="text-xs-right">
+				<h6> Version: {{ version }} </h6>
 			</div>
 		</v-container>
 	</v-footer>
 </template>
 
 <script>
-import { VFooter } from "vuetify";
+import { VFooter, VBtn } from "vuetify";
 import { VContainer, VFlex, VLayout } from "vuetify/es5/components/VGrid";
 import { mapGetters } from "vuex";
 
 export default {
 	name: "Footer",
+	data() {
+		return {
+			version: VERSION
+		};
+	},
 	components: {
 		VContainer,
 		VFlex,
 		VLayout,
-		VFooter
+		VFooter,
+		VBtn
 	},
 	computed: mapGetters({
 		drawer: "drawer"
@@ -93,47 +112,4 @@ export default {
 </script>
 
 <style lang="stylus">
-
-	.footer {
-
-
-		p {
-			margin-top 15px
-			text-align justify
-		}
-
-		.links {
-			margin 15px;
-			margin-bottom 10px
-			
-			li {
-				padding 12px;
-				display inline
-			}
-		}
-
-		.follow 
-		{
-			position relative
-			top 50%
-			transform: translateY(-50%);
-			.social {
-				margin: 20px
-				display inline-block
-				width 55px;
-				height: 55px;
-				padding-top: 10px;
-				border-radius: 100%
-				
-				&:hover {
-					opacity: 0.9;
-				}
-			}
-
-		   
-		}
-
-		
-	}
-
 </style>
