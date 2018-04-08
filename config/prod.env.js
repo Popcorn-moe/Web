b6af633e37bbb10c13e704b1e045a4b09611d83f
Duarte David <deltaduartedavid@gmail.com>
2018-04-08 12:48:37 +0200

    Change prod default urls

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,8 +2,8 @@ module.exports = {
 	NODE_ENV: '"production"',
 	AUTH_URL: process.env.AUTH_URL
 		? `'${process.env.AUTH_URL}'`
-		: "'http://localhost:3031'",
+		: "'https://auth.popcorn.moe'",
 	API_URL: process.env.API_URL
 		? `'${process.env.API_URL}'`
-		: "'http://localhost:3030'"
+		: "'https://api.popcorn.moe'"
 };
